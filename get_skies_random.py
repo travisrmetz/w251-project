c8707ee67134f07b1536c4d0a2f1d60cecd200b7
Travis Metz <travisrmetz@gmail.com>
2020-06-21 11:42:02 -0400

    added random grid

--- get_skies_random.py
+++ get_skies_random.py
@@ -2,6 +2,7 @@ import yaml
 import subprocess
 import geopy
 from sky_helper import next_lat, next_long,get_file_name
+import random
 
 def main():
     """
@@ -26,44 +27,39 @@ def main():
     fov = config_data['fov']
     latstart = config_data['latstart']
     latend=config_data['latend']
-    meters_lat_slice=config_data['meters_lat_slice']
+    #meters_lat_slice=config_data['meters_lat_slice']
     longstart = config_data['longstart']
     longend=config_data['longend']
-    meters_long_slice=config_data['meters_long_slice']
+    #meters_long_slice=config_data['meters_long_slice']
     date = config_data['date']
+    number_images=config_data['number_images']
 
     # Write the ssc file
     image_index = 0
     with open(script_path + 'get_multi_sky.ssc', 'w') as f:
         # TODO:  Allow time to be read in as a list (move the following
         #        line to the loop).
-        #        Loop through time?
 
-        i=latstart
-        j=longstart
-        image_counter=0
-        while j<longend:
-            while i<latend:
-                f.write('core.setDate("{}", "utc");\n'.format(date))
-                f.write('LandscapeMgr.setFlagAtmosphere(true);\n')
-                f.write('StelMovementMgr.zoomTo({},0);\n'.format(fov))
-                f.write('core.setGuiVisible(false);\n')
-                f.write('StarMgr.setLabelsAmount(0);\n')
-                f.write('SolarSystem.setFlagLabels(false);\n')
-                f.write('MeteorShowers.setEnableMarker(false);\n')
-                f.write('core.setObserverLocation({}, {}, 15, 1, "Ocean", "Earth");\n'.format(i, j))
-                # The next statement needs to be repeated to generate stable images.
-                # It seems that Stellarium doesn't like altitudes of 90 degrees.
-                f.write('core.moveToAltAzi({}, {});\n'.format(alt, azi))
-                file_name=get_file_name(i,j,date)
-                f.write('core.screenshot("{}", invert=false, dir="{}", overwrite=true);\n'.format(get_file_name(i,j,date), image_dir))
-                i=next_lat(i,j,meters_lat_slice)
-                image_counter+=1
-            i=latstart
-            j=next_long(i,j,meters_long_slice)
-        
-        print('Creating #:', image_counter)
-    
+        for x in range(number_images):
+            #pick random location within grid
+            i=random.uniform(latstart,latend)
+            j=random.uniform(longstart,longend)
+            print (x,i,j)
+            #write script for gathering image from that location
+            f.write('core.setDate("{}", "utc");\n'.format(date))
+            f.write('LandscapeMgr.setFlagAtmosphere(true);\n')
+            f.write('StelMovementMgr.zoomTo({},0);\n'.format(fov))
+            f.write('core.setGuiVisible(false);\n')
+            f.write('StarMgr.setLabelsAmount(0);\n')
+            f.write('SolarSystem.setFlagLabels(false);\n')
+            f.write('MeteorShowers.setEnableMarker(false);\n')
+            f.write('core.setObserverLocation({}, {}, 15, 1, "Ocean", "Earth");\n'.format(i, j))
+            # The next statement needs to be repeated to generate stable images.
+            # It seems that Stellarium doesn't like altitudes of 90 degrees.
+            f.write('core.moveToAltAzi({}, {});\n'.format(alt, azi))
+            file_name=get_file_name(i,j,date)
+            f.write('core.screenshot("{}", invert=false, dir="{}", overwrite=true);\n'.format(get_file_name(i,j,date), image_dir))
+            
     # Open Stellarium and run the script
     proc_stellarium = subprocess.Popen(['stellarium', '--startup-script', 'get_multi_sky.ssc', '--screenshot-dir', image_dir], stdout=subprocess.PIPE)
 
