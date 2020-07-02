fc2daff3c9f740f2545dcd32641da2885085f754
Travis Metz <travisrmetz@gmail.com>
2020-07-02 19:34:10 -0400

    trm fussing

--- get_skies.sh
+++ get_skies.sh
@@ -3,7 +3,7 @@
 #s3fs rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
 #s3fs -o nonempty rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
 
-google-drive-ocamlfuse /data/stars
+google-drive-ocamlfuse /w251-project/stars
 python3 get_skies_random.py #does a random selection of set # of locations
 #python3 get_arbitrary_skies.py [does a grid search]
 
