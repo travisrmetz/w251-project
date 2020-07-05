d71ced64e937445308e99ab5f1284674215d338a
Travis Metz <travisrmetz@gmail.com>
2020-07-05 11:04:25 -0400

    updated get_skies

--- get_skies.sh
+++ get_skies.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-#s3fs rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
-#s3fs -o nonempty rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
-#google-drive-ocamlfuse /w251-project/stars
+#ssc_generator.yml has default values to use for items
 
-#use ssc_generator.yml file to figure out box and number
+#first delete all images on local drive and up in S3
+python3 delete_images.py
+
+#then generate random images from box defined by ssc_generatory.yml
 python3 get_skies_random.py 
 
 #uploads to s3 bucket
+#if stellarium does not quit properly then this needs to be run by hand
 python3 upload_images_s3.py
 
+
+
+#OLD AND NOT USED
+#s3fs rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
+#s3fs -o nonempty rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
+#google-drive-ocamlfuse /w251-project/stars
 #if doing grid
 #python3 get_arbitrary_skies.py
 
