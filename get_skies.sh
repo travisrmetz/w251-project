c5964d828e85e902c4406209a688b8dee85aa119
Travis Metz <travisrmetz@gmail.com>
2020-07-05 08:17:28 -0400

    clean up commit

--- get_skies.sh
+++ get_skies.sh
@@ -2,8 +2,14 @@
 
 #s3fs rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
 #s3fs -o nonempty rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
-
 #google-drive-ocamlfuse /w251-project/stars
-python3 get_skies_random.py #does a random selection of set # of locations
-#python3 get_arbitrary_skies.py [does a grid search]
+
+#use ssc_generator.yml file to figure out box and number
+python3 get_skies_random.py 
+
+#uploads to s3 bucket
+python3 upload_images_s3.py
+
+#if doing grid
+#python3 get_arbitrary_skies.py
 
