0777d42d400cf348775721b3e7259f81a40e4809
Travis Metz <travisrmetz@gmail.com>
2020-07-27 11:28:52 -0400

    melding inference to GT model

--- inference/inference.sh
+++ inference/inference.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sync; echo 2 > /proc/sys/vm/drop_caches
-sync; echo 3 > /proc/sys/vm/drop_caches
+#sync; echo 1 > /proc/sys/vm/drop_caches
+#sync; echo 2 > /proc/sys/vm/drop_caches
+#sync; echo 3 > /proc/sys/vm/drop_caches
 
 #maximizes performance - can run from container?
 sudo jetson_clocks
