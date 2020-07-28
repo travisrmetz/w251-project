876ad16547541ee754e3ebc1558dd23f67d700cd
Travis Metz <travisrmetz@gmail.com>
2020-07-27 21:12:32 -0400

    fixed inference.py

--- inference/inference.py
+++ inference/inference.py
@@ -1,26 +1,19 @@
-
-
-#load model
-#take in image
-#take in dt
-#do predict
-#print out lat/long
-#show image?
-#how do I store S3 credentials without putting up on github?
-
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
 import os
 import boto3
 import io
-import haversine as hs
+#import haversine as hs
 from geopy.distance import geodesic
+import random as random
+from inference_functions import load_image,get_labels,normalize_times
+import yaml
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(gpus[0], True)
 
 def load_model_from_s3():
     bucket_name='w251-final-project-model'  
@@ -49,26 +42,25 @@ def load_model_from_s3():
     print('Loaded model from S3')
     return model
 
-def load_image(image_path, dim=(224,224)):
-    """
-    Loads a single image as a Numpy array and resizes it as
-    desired.  The default dimensions are consistent with
-    those expected by the VGG models.  
+# def load_image(image_path, dim=(224,224)):
+#     """
+#     Loads a single image as a Numpy array and resizes it as
+#     desired.  The default dimensions are consistent with
+#     those expected by the VGG models.  
 
-    Args:
-        file_name:  file to convert to image
+#     Args:
+#         file_name:  file to convert to image
 
-    dim: Two-element tuple giving the desired height
-         and width of the processed image
+#     dim: Two-element tuple giving the desired height
+#          and width of the processed image
 
-    Returns:
-    image:  A single-channel Numpy array
-    """
-    image = cv2.imread(image_path, 0)
-    image = cv2.resize(image, dim)
-    plt.imshow(image)
-    image_array=np.array(image) / 255
-    return image_array
+#     Returns:
+#     image:  A single-channel Numpy array
+#     """
+#     image = cv2.imread(image_path, 0)
+#     image = cv2.resize(image, dim)
+#     image_array=np.array(image) / 255
+#     return image_array
 
 def scale_down(numbers):
     top=max(numbers)
@@ -86,41 +78,85 @@ def scale_up(numbers,top,bottom):
     revised=[x+middle for x in revised]
     return revised
 
-#get model from s3
-model=load_model_from_s3()
+def main():
+    
+    #get configurationvalues
+    with open('inference.yml') as config_file:
+        config_data = yaml.load(config_file)
+    image_dir = config_data['image_dir']
+    latstart = config_data['latstart']
+    latend=config_data['latend']
+    longstart = config_data['longstart']
+    longend=config_data['longend']
+    dtstart = config_data['dtstart']
+    dtend=config_data['dtend']
+    
+    
+    #get model from s3
+    #model=load_model_from_s3()
 
-#get file and do preprocessing and show it
-#take in argument
-print('Loading image')
-image_array=load_image('/inference/test.png')
-print('Loaded image with shape:',image_array.shape)
-X_test=np.expand_dims(image_array,axis=3)
-X_test=np.expand_dims(X_test,axis=0)
-print('Shape of X_test',X_test.shape)
-
-#do prediction
-print('Ready to do prediction')
-y_hat = model.predict(X_test)
+    #get model from local directory
+    #local_model_dir="/tmp/inference_model"
+    #model=tf.keras.models.load_model(local_model_dir)
+    #print (model.summary())
 
 
-#output results
-y_hat_lat=y_hat[0]
-y_hat_long=y_hat[1]
-toplat,bottomlat,toplong,bottomlong=50,30,-50,-70
-y_hat_lat=scale_up(y_hat_lat,toplat,bottomlat)
-y_hat_long=scale_up(y_hat_long,toplong,bottomlong)
+    #get file and do preprocessing and show it
+    #take in argument
 
-actual_lat=34.95968170768319
-actual_long=-65.96097974743151
+    #select in random image
+    path, dirs, files = next(os.walk(image_dir))
+    file_count = len(files)
+    random_file=files[random.randint(0,file_count)]
+    print('Random file selected:', random_file)
 
-point1=(y_hat_lat[0],y_hat_long[0])
-point2=(actual_lat,actual_long)
- 
-loss_nm=geodesic(point1,point2).nautical
 
-print('Estimated latitude, longitude:',y_hat_lat[0],',',y_hat_long[0])
-print('Actual latitude, longitude:',actual_lat,',',actual_long)
-print('Error in nautical miles:',loss_nm)
+    #load image
+    print('Loading image')
+    image_array=load_image(os.path.join(image_dir,random_file))
+    print('Loaded image with shape:',image_array.shape)
+    plt.imshow(image_array)
 
-#this does not work due to bug in tf 2.0 -- need to find way to upgrade to tf 2.1 or above
-#print(model.summary())
+
+    #get labels to test image
+    y_lat,y_long,time=get_labels(random_file)
+    print('Test image lat,long,time:', y_lat,y_long,time)
+
+    #process proper image dimensions
+    X_test=np.expand_dims(image_array,axis=3)
+    X_test=np.expand_dims(X_test,axis=0)
+    print('Shape of X_test',X_test.shape)
+
+    #process time into TF input
+    #converted to some sort of DT?
+    T_test=normalize_times(time,dtstart,dtend)
+    print ('T_test',T_test)
+
+    #do prediction
+    print('Ready to do prediction')
+    y_hat = model.predict([X_test,T_test])
+
+
+    #output results
+    y_hat_lat=y_hat[0]
+    y_hat_long=y_hat[1]
+        
+    y_hat_lat=scale_up(y_hat_lat,latend,latstart)
+    y_hat_long=scale_up(y_hat_long,longend,longstart)
+
+    point1=(y_hat_lat[0],y_hat_long[0])
+    point2=(y_lat,y_long)
+    
+    loss_nm=geodesic(point1,point2).nautical
+
+    print('Estimated latitude, longitude:',y_hat_lat[0],',',y_hat_long[0])
+    print('Actual latitude, longitude:',y_lat,',',y__long)
+    print('Error in nautical miles:',loss_nm)
+
+    #this does not work due to bug in tf 2.0 -- need to find way to upgrade to tf 2.1 or above
+    #print(model.summary())
+
+
+
+if __name__ == "__main__":
+    main()
