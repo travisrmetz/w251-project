ef93f25db924c1d8f07775508932a1db34797f55
Travis Metz <tmetz@ischool.berkeley.edu>
2020-08-07 07:41:14 -0400

    repo cleanup

--- get_skies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-s3fs rza-cos-standard-l5d /data/stars/images -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=/data/credentials_file
-
-python3 get_arbitrary_skies.py
-
